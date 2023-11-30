5da9960708af67c453d9cf8d6db4c9690419f4eb
ehdus5605 <150339851+ehdus5605@users.noreply.github.com>
2023-11-30 15:29:49 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231130/연속부분수열일까/contiguous-array-or-not.c
+++ 231130/연속부분수열일까/contiguous-array-or-not.c
@@ -1,64 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 void input(int *p, int k) {
-    for (int i = 0; i < k; i++)
+    int i;
+    for(i=0;i<k;i++) {
         scanf("%d", &p[i]);
+    }
 }
 
-
-int isSubsequence(int *x, int *y, int n, int m) {
-    
-    int start = -1;
-    
-    int j = 0;
-
-    
-    for (int i = 0; i < n; i++) {
-        if (x[i] == y[0]) {
-            start = i;
-            break;
+int fun(int *x, int *y, int n, int m) {
+    int i, j, start;
+    for(start = 0; start <= n - m; start++) {
+        if(x[start] == y[0]) {
+            for(i = 1; i < m; i++) {
+                if(x[start + i] != y[i]) {
+                    break;
+                }
+            }
+            if(i == m) {
+                return 1;
+            }
         }
     }
-
-    
-    if (start == -1)
-        return 0;
-
-    
-    for (int i = start; i < start + m; i++) {
-        if (x[i] != y[j++])
-            return 0;
-    }
-
-    return 1;
+    return 0;
 }
 
 int main() {
     int n, m;
     int *p1, *p2;
-    int result;
-
-    
+    int sw;
     scanf("%d %d", &n, &m);
-    p1 = (int *)malloc(sizeof(int) * n);
-    p2 = (int *)malloc(sizeof(int) * m);
+    p1=(int *)malloc(sizeof(int)*n);
+    p2=(int *)malloc(sizeof(int)*m);
     input(p1, n);
     input(p2, m);
-
-    
-    result = isSubsequence(p1, p2, n, m);
-
-    
-    if (result == 1)
+    sw = fun(p1, p2, n, m);
+    if(sw==1) {
         printf("Yes\n");
-    else
+    }
+    else {
         printf("No\n");
-
-    
+    }
     free(p1);
     free(p2);
-
     return 0;
 }
