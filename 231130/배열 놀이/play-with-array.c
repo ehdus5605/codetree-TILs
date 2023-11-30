883f39eadb1e71d56647626e107465ab1c75effc
ehdus5605 <150339851+ehdus5605@users.noreply.github.com>
2023-11-30 14:31:46 +0900

    Title: 배열 놀이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231130/배열 놀이/play-with-array.c
+++ 231130/배열 놀이/play-with-array.c
@@ -1,49 +1,51 @@
 #include <stdio.h>
-
-int main() {
-	int n, q;
-    
-	scanf("%d %d", &n, &q);
-	int arr[100];
-	
-	for(int i = 0; i < n; i++)
-		scanf("%d", &arr[i]);
-	
-	for(int i = 0; i < q; i++) {
-		int qt;
-		scanf("%d", &qt);
-		
-		if(qt == 1) {
-			int a;
-			scanf("%d", &a);
-			
-			printf("%d\n", arr[a - 1]);
-		}
-		else if(qt == 2) {
-			int a;
-			scanf("%d", &a);
-			
-			int idx = -1;
-			for(int j = 0; j < n; j++) {
-				if(arr[j] == a) {
-					idx = j;
-					break;
-				}
-			}
-			
-			printf("%d\n", idx + 1);
-		}
-		else {
-			int a, b;
-			scanf("%d %d", &a, &b);
-			
-			for(int j = a - 1; j < b; j++) {
-				printf("%d ", arr[j]);
-			}
-			printf("\n");
-		}
-	}
-	
+#include <stdlib.h>
+int n;
+void proc(int a,int b,int c,int *p)
+{
+    int i;
+    if(a==1)
+    {
+        printf("%d\n",p[b-1]);
+    }
+    else if(a==2)
+    {
+        for(i=0;i<n;i++)
+        {
+            if(p[i]==b){
+                printf("%d\n",i+1);
+                return;}
+        }
+        printf("0\n");
+    }
+    else
+    {
+        for(i=b;i<=c;i++)
+        {
+            
+                printf("%d ",p[i-1]);
+        }
+        printf("\n");
+    }
+}
+int main() 
+{
+    int m,*p,a,b,c,i;
+    scanf("%d %d",&n,&m);
+    //p(int *)malloc(sizeof(int)*n);
+    p = (int *)malloc(sizeof(int) * n);
+    for(i=0;i<n;i++) {
+        scanf("%d",&p[i]);
+    }
+    for(i=0;i<m;i++) {
+        scanf("%d",&a);
+        if(a==3)
+            scanf("%d %d",&b,&c);
+        else
+            scanf("%d",&b);
+        proc(a,b,c,p);
+    }
+    free(p);
     return 0;
 }
 
