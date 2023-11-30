2b6f843be24a151cc8aad8b8136ea72c5424c598
ehdus5605 <150339851+ehdus5605@users.noreply.github.com>
2023-11-30 13:38:43 +0900

    Title: 007, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231130/007/007.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char p;
+    scanf("%s", &p);
+    printf("secret code : codetree\n");
+    printf("meeting point : L\n");
+    printf("time : 13");
+    return 0;
+}
